c48339827820329624b1ff709af0d606899d0d10
Stuti Sharma <stutis070@gmail.com>
2025-04-03 17:58:12 +0530

    api creation and database config

--- tweet/urls.py
+++ tweet/urls.py
@@ -1,9 +1,17 @@
 from . import views
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import TweetViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+router=DefaultRouter()
+router.register(r'tweets', TweetViewSet)
+
 urlpatterns = [
     path('', views.tweet_list, name='tweet_list'),
     path('create/', views.tweet_create, name='tweet_create'),
     path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
     path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
     path('register/', views.register, name='register'),
+    path('api/',include(router.urls)),
+    path('api/token/',obtain_auth_token, name='api_token_auth'),
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -4,7 +4,12 @@ from .forms import TweetForm, UserRegistrationForm
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
-
+from rest_framework import viewsets, permissions
+from .serializers import TweetSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 def index(request):
@@ -61,3 +66,25 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request, 'registration/register.html', {'form':form} )
+
+class TweetViewSet(viewsets.ModelViewSet):
+    queryset=Tweet.objects.all().order_by('-created_At')
+    serializer_class=TweetSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly]
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user) 
+    @api_view(['GET', 'POST'])
+    def tweet_list(request):
+        if request.method == 'GET':
+            tweets = Tweet.objects.all()
+            serializer = TweetSerializer(tweets, many=True)
+            return Response(serializer.data)
+
+        if request.method == 'POST':
+            serializer = TweetSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
